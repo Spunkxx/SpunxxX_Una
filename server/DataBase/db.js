366b64e761602b176e64340940c40151a1f70be7
Dominic John Bunal <djbunal43@gmail.com>
2023-09-07 18:22:27 +0800

    Added the Data base For Una_Test

--- server/DataBase/db.js
+++ server/DataBase/db.js
@@ -2,56 +2,39 @@ const mysql = require('mysql')
 const dotenv = require('dotenv')
 dotenv.config();
 
-// const db = mysql.createConnection({
-//     port: process.env.PORT,
-//     host: process.env.HOST,
-//     user: process.env.USER,
-//     password: process.env.PASSWORD,
-//     database: process.env.DATABASE,
-//   });
-//   db.connect((err) => {
-//       if(err) {
-//           throw err;
-//       }
-//       console.log('Conneted to the DataBase')
-//   });
-  
-// module.exports = db;
-
-
 // Flast
-
-const db = mysql.createConnection({
+const dbFlast = mysql.createConnection({
     port: process.env.PORT,
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
   });
-  db.connect((err) => {
+  dbFlast.connect((err) => {
       if(err) {
           throw err;
       }
       console.log('Conneted to the FLAST')
   });
   
-module.exports = db;
 
-
-// Una
-// const db = mysql.createConnection({
-//     host: process.env.HOST,
-//     user: process.env.USER,
-//     password: process.env.PASSWORD,
-//     database: process.env.DATABASE,
-//   });
-//   db.connect((err) => {
-//       if(err) {
-//           throw err;
-//       }
-//       console.log('Conneted to the UNA')
-//   });
+//  Una
+  const dbUna = mysql.createConnection({
+    port: process.env.PORT_UNA,
+    host: process.env.HOST_UNA,
+    user: process.env.USER_UNA,
+    password: process.env.PASSWORD_UNA,
+    database: process.env.DATABASE_UNA,
+  });
+  dbUna.connect((err) => {
+      if(err) {
+          throw err;
+      }
+      console.log('Conneted to the UNA')
+  });
   
-// module.exports = db;
+
+module.exports = {dbFlast,dbUna};
+
 
 
